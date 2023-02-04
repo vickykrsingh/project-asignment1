fb264d72753aefab36a9c62e7be3d583fd24f1e6
Deadly844119 <103536456+Deadly844119@users.noreply.github.com>
2023-02-04 13:54:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = document.getElementById('menu')
+const order = document.getElementById('order')
+const bar = document.getElementById('bar')
+
+const showMenu = () => {
+    if(menu.style.left=="0%" && order.style.display=="block")
+    {
+        
+        menu.style.left="-100%" ;
+        order.style.display="none";
+    }
+    else{
+        menu.style.left="0%";
+        order.style.display="block"
+    }
+}
+
+bar.addEventListener('click',showMenu)
